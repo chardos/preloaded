3bd45cccbc1f8ceeee9b3a1acb6beb3247f92f6a
Greg Dziemidowicz <greg@cogent.co>
2015-03-28 12:34:37 +1100

    take address

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -15,7 +15,7 @@ class ChargesController < ApplicationController
         :customer    => customer.id,
         :amount      => @amount,
         :description => 'Rails Stripe customer',
-        :currency    => 'usd'
+        :currency    => 'eur'
     )
 
   rescue Stripe::CardError => e
